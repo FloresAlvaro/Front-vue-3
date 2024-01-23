10954a1ef51e579896cf1bd5cbe7730961fdefc3
FloresAlvaro <alvarofloresfarfan2802@gmail.com>
2024-01-23 11:19:57 -0400

    add a card component

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+
+import Card from "./components/Card.vue";
+
+export default {
+  components: { Card },
+};
+
+</script>
+
+<template>
+  <h1>Componente</h1>
+  <br/>
+  <card/>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card-container">
+    <img class="card-img" src="../assets/logo.svg" alt="Vue js" />
+    <h3 class="card-label">Vue js</h3>
+  </div>
+</template>
+
+<style scoped>
+    .card-container {
+        text-align: center;
+        background-color: #555555;
+        border: 1.5px solid #fff;
+        border-radius: 5px;
+        padding: 5px;
+    }
+    .card-img{
+        height: 150px;
+        width: 150px;
+        margin: 5px;
+        padding: 5px;
+    }
+    .card-label{
+        color: #ffffff;
+        background-color: #4776bd;
+        border-radius: 5px;
+        border: 0.5px solid #fff;
+        margin: 5px;
+    }
+</style>
